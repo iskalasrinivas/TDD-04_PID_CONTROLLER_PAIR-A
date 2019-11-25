be0c7e42a9ce7623488bc94dd97aba3a6e170a39
iskalasrinivas <iskalasrinivas3@gmail.com>
2019-11-25 13:51:34 -0500

    [app/main.cpp] changed the object creation to smart pointer and fixed cpplint errors.

--- app/main.cpp
+++ app/main.cpp
@@ -2,19 +2,16 @@
  *  @copyright 2019
  *  @copyright GNU Public License
  *  @file main.cpp - app
- *  Part - 1
- *  @author Sandeep Kota Sai Pavan-driver
- *  @author Raj Prakash Shinde-navigator
- *  Part - 2
- *  @author  Raja Iskala - driver
- *  @author Yashaarth Todi-navigator 
- *  @date 09/25/2019
+ *  @author Raja Srinivas Iskala
+ *  @date 11/24/2019
  *  @version 1.0
  *  @brief Main file containing the main() method.
  */
 
+#include <memory>
 #include <iostream>
-#include "pid.hpp"
+#include "include/pid.h"
+#include "include/basePID.h"
 
 /**
  *  @brief Main function to call methods
@@ -22,21 +19,21 @@
  *  @return 0
  */
 int main() {
-PidController o;
-double actualVelocity = 10;
-double target = 30;
-int count = 0;
-int bound = 1;
-std::cout << "p gain :" << o.getkp() << std::endl;
-std::cout << "i gain :" << o.getki() << std::endl;
-std::cout << "d gain :" << o.getkd() << std::endl;
-std::cout << "dt gain:" << o.dt << std::endl;
+  // use unique pointer to make base class pointer to derived class object.
+  std::unique_ptr<BasePID> o = std::make_unique<PidController>();
+  double actualVelocity = 10;
+  double target = 30;
+  int count = 0;
+  int bound = 1;
+  std::cout << "p gain :" << o->getkp() << std::endl;
+  std::cout << "i gain :" << o->getki() << std::endl;
+  std::cout << "d gain :" << o->getkd() << std::endl;
 //  Check convergance of the designed controller
-while(abs(actualVelocity - target) > bound) {
-actualVelocity = o.compute(actualVelocity, target);
-count++;
-}
-std::cout << "New velocity: " << actualVelocity << " achieved in "
-<< count << " cycles" << std::endl;
-return 0;
+  while (abs(actualVelocity - target) > bound) {
+    actualVelocity = o->compute(actualVelocity, target);
+    count++;
+  }
+  std::cout << "New velocity: " << actualVelocity << " achieved in " << count
+            << " cycles" << std::endl;
+  return 0;
 }
