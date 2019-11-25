f45e7608c86e1bcb9490d63e60f64e25ed7d3dda
iskalasrinivas <iskalasrinivas3@gmail.com>
2019-11-25 14:43:24 -0500

    [include/pid.hpp] changed header file name for pid.hpp and basePID.hpp.

--- include/pid.hpp
+++ include/pid.hpp
@@ -1,95 +1,92 @@
 /**
  *  @copyright 2019
  *  @copyright GNU Public License
- *  @file pid.hpp
- *  Part - 1
- *  @author Sandeep Kota Sai Pavan-driver
- *  @author Raj Prakash Shinde-navigator
- *  Part - 2
- *  @author  Raja Iskala - driver
- *  @author Yashaarth Todi-navigator 
- *  @date 09/25/2019
+ *  @file pid.h
+ *  @author Raja Srinivas Iskala
+ *  @date 11/24/2019
  *  @version 1.0
- *  @brief Class defination of PidController
+ *  @brief Class definition of PidController
  */
 
-#ifndef INCLUDE_PID_HPP_
-#define INCLUDE_PID_HPP_
+#ifndef INCLUDE_PID_H_
+#define INCLUDE_PID_H_
+
+#include "basePID.hpp"
 
 /**
  *  @brief Class containing the parameters of the 
- *  PID Controller.
+ *  PID Controller which derives from interface BasePID.
  */
-class PidController {
- private:
-    double kp_, ki_, kd_;
+class PidController : public BasePID {
 
  public:
-    double prevError, integral, derivative, dt;
-    /**
-    *  @brief Constructor for class PidController
-    */
-    PidController();
-
-    /**
-    *  @brief Destructor for class PidController
-    */
-    ~PidController();
-
-   /**
-    *  @brief Function to compute output of the PID Controller
-    *  given the value of the previous state and goal state.
-    *  @param int prevValue Previous state of the control parameter
-    *  @param int setPoint Goal state of the control parameter
-    *  @return int prevValue + output Updated output of the PID 
-    *  controller block.
+  double prevError, integral, derivative, dt;
+  /**
+   *  @brief Constructor for class PidController
    */
-    double compute(double prevValue, double setPoint);
+  PidController();
 
-    /**
-    *  @brief Function to get kp_ attribute of class PidController
-    *  @param none
-    *  @return double
-    */
-     double getkp();
+  /**
+   *  @brief Destructor for class PidController
+   */
+  ~PidController();
 
-    /**
-    *  @brief Function to get ki_ attribute of class PidController
-    *  @param none
-    *  @return double 
-    */
-     double getki();
+  /**
+   *  @brief Function to compute output of the PID Controller
+   *  given the value of the previous state and goal state.
+   *  @param int prevValue Previous state of the control parameter
+   *  @param int setPoint Goal state of the control parameter
+   *  @return int prevValue + output Updated output of the PID
+   *  controller block.
+   */
+  double compute(double prevValue, double setPoint) override;
 
-    /**
-    *  @brief Function to get kd_ attribute of class PidController
-    *  @param none
-    *  @return double 
-    */
-     double getkd();
+  /**
+   *  @brief Function to get kp_ attribute of class PidController
+   *  @param none
+   *  @return double
+   */
+  double getkp() override;
 
-    /**
-    *  @brief Function to set kp_ attribute of class PidController
-    *  @param double p
-    *  @return boolean 
-    */
-     bool setkp(double p);
+  /**
+   *  @brief Function to get ki_ attribute of class PidController
+   *  @param none
+   *  @return double
+   */
+  double getki() override;
 
-    /**
-    *  @brief Function to set ki_ attribute of class PidController
-    *  @param double i
-    *  @return boolean 
-    */
-     bool setki(double i);
+  /**
+   *  @brief Function to get kd_ attribute of class PidController
+   *  @param none
+   *  @return double
+   */
+  double getkd() override;
 
-    /**
-    *  @brief Function to set kd_ attribute of class PidController
-    *  @param double d
-    *  @return boolean 
-    */
+  /**
+   *  @brief Function to set kp_ attribute of class PidController
+   *  @param double p
+   *  @return boolean
+   */
+  bool setkp(double p) override;
 
-     bool setkd(double d);
+  /**
+   *  @brief Function to set ki_ attribute of class PidController
+   *  @param double i
+   *  @return boolean
+   */
+  bool setki(double i) override;
+
+  /**
+   *  @brief Function to set kd_ attribute of class PidController
+   *  @param double d
+   *  @return boolean
+   */
+
+  bool setkd(double d) override;
+
+ private:
+  double kp_, ki_, kd_;
 };
 
-#endif  // INCLUDE_PID_HPP_
-
+#endif  // INCLUDE_PID_H_
 
